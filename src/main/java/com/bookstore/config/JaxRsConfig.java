7b6ec02d34884e8a6195c72ffe6edfe54087c0e6
PoornaManakal <166126968+PoornaManakal@users.noreply.github.com>
2025-06-26 23:31:53 +0530

    initial commit with pom.xml update

---
+++ src/main/java/com/bookstore/config/JaxRsConfig.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bookstore.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class JaxRsConfig extends Application {
+    // No need to override anything if you only use basic features
+}
+
