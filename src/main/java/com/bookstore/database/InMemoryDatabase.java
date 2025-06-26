b7c55815d843d00f92bdaba4be963b96e69cefd4
PoornaManakal <166126968+PoornaManakal@users.noreply.github.com>
2025-06-26 23:56:44 +0530

    Implemented in-memory storage for books

---
+++ src/main/java/com/bookstore/database/InMemoryDatabase.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bookstore.database;
+
+import com.bookstore.model.*;
+
+import java.util.*;
+
+public class InMemoryDatabase {
+    public static Map<Integer, Book> books = new HashMap<>();
+    public static Map<Integer, Author> authors = new HashMap<>();
+    public static Map<Integer, Customer> customers = new HashMap<>();
+    public static Map<Integer, List<CartItem>> carts = new HashMap<>();
+    public static Map<Integer, List<Order>> orders = new HashMap<>();
+}
+
